c15dbcd3df5a55a04b4ea6845a0096bd4c6e0d31
Katarzyna Kawala <kasia.kawala@90poe.io>
2025-10-05 22:29:06 +0200

    ci: installing chromium in ci

--- playwright.config.ts
+++ playwright.config.ts
@@ -34,11 +34,7 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { 
-        ...devices['Desktop Chrome'],
-        // Use system Chrome in CI for speed
-        channel: process.env.CI ? 'chrome' : undefined 
-      },
+      use: { ...devices['Desktop Chrome'] },
     },
   ],
 
